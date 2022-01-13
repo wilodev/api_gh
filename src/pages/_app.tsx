a581bc878962fa665e86c83e58b00ed09c89bd0a
wilodev <wperez@cintanegra.net>
2022-01-13 15:35:18 -0500

    feat: :sparkles: Add Bootstrap
    Se añade bootstrap como framewok css para el proyecto

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+// Se añade la librería de Bootstrap
+import "bootstrap/dist/css/bootstrap.css";
+// Se añade la hoja de estilos custom global para el proyecto
+import "../styles/globals.css";
+// Se instacia el tipo para las props de NextJs
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
